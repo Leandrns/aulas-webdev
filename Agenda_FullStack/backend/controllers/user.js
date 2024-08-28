94ef5bc2c93c9c5af1334fc18064c16ae7929cb1
Leandrns <leandro2005souza@gmail.com>
2024-08-28 11:39:17 -0300

    Finalizando backend agenda fullstack

---
+++ Agenda_FullStack/backend/controllers/user.js
@@ -0,0 +1,45 @@
+import { db } from "../db.js";
+
+export const getUsers = (_, res) => {
+    const q = "SELECT * FROM agenda";
+    db.query(q, (err, data) => {
+        if (err) return res.json(err);
+        return res.status(200).json(data);
+    });
+};
+
+export const addUser = (req, res) => {
+    const q = "INSERT INTO agenda (`nome`, `telefone`, `endereco`, `email`) VALUES(?)";
+    const values = [
+        req.body.nome,
+        req.body.telefone,
+        req.body.endereco,
+        req.body.email,
+    ];
+    db.query(q, [values], (err) => {
+        if (err) return res.json(err);
+        return res.status(200).json("Registro criado com sucesso");
+    });
+};
+
+export const updateUser = (req, res) => {
+    const q = "UPDATE agenda SET `nome` = ?, `telefone` = ? , `endereco` = ?, `email` = ? WHERE `id` = ? ";
+    const values = [
+        req.body.nome,
+        req.body.telefone,
+        req.body.endereco,
+        req.body.email,
+    ];
+    db.query(q, [...values, req.params.id], (err) => {
+        if (err) return res.jason(err);
+        return res.status(200).json("Registro atualizado com sucesso");
+    });
+};
+
+export const deleteUser = (req, res) => {
+    const q = "DELETE FROM agenda WHERE `id` = ?";
+    db.query(q, [req.params.id], (err) => {
+        if (err) return res.json(err);
+        return res.status(200).json("Registro deletado com sucesso");
+    });
+};
