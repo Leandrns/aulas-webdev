ae394fb746e7137035585e59aab5d1f5d0edf8e8
Leandrns <leandro2005souza@gmail.com>
2024-10-09 11:33:57 -0300

    add livros

--- Novas aulas/React/store-books/src/components/Pesquisa/dadosPesquisa.js
+++ Novas aulas/React/store-books/src/components/Pesquisa/dadosPesquisa.js
@@ -1,4 +1,13 @@
 import livro from '../../imagens/livro.png'
+import livro6 from '../../livrosImg/Acessibilidade na Web.jpg'
+import livro7 from '../../livrosImg/Bootstrap4.png'
+import livro8 from '../../livrosImg/Colecao Cangaceiro JavaScript.png'
+import livro9 from '../../livrosImg/Coletânea Front-End.png'
+import livro10 from '../../livrosImg/Guia Front-End.png'
+import livro11 from '../../livrosImg/HTML5 e CSS3.png'
+import livro12 from '../../livrosImg/Progressive Web Apps.jpg'
+import livro13 from '../../livrosImg/Sass.png'
+import livro14 from '../../livrosImg/Turbine seu CSS.png'
 
 export const livros = [
     {nome: 'Liderança em Design', id: 1, src:livro},
@@ -6,4 +15,13 @@ export const livros = [
     {nome: 'JavaScript Assertivo', id: 3, src:livro},
     {nome: 'Cangaceiro em JavaScript', id: 4, src:livro},
     {nome: 'Aventureiros do Java', id: 5, src:livro},
+    {nome: 'Acessibilidade na Web', id: 6, src:livro6},
+    {nome: 'Bootstrap 4', id: 7, src:livro7},
+    {nome: 'Coleção Cangaceiro em JavaScript', id: 8, src:livro8},
+    {nome: 'Coletânea Front-End', id: 9, src:livro9},
+    {nome: 'Guia Front-End', id: 10, src:livro10},
+    {nome: 'HTML5 e CSS3', id: 11, src:livro11},
+    {nome: 'Progressive Web Apps', id: 12, src:livro12},
+    {nome: 'Sass', id: 13, src:livro13},
+    {nome: 'Turbine seu CSS', id: 14, src:livro14},
 ]
