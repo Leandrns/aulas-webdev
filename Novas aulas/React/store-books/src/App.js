a9a23a666b88b1208acfa281eaff9a00f26f4394
Leandrns <leandro2005souza@gmail.com>
2024-10-09 11:05:44 -0300

    aula 17

--- Novas aulas/React/store-books/src/App.js
+++ Novas aulas/React/store-books/src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
-import logo from './logo.svg';
+import Header from './components/Header'
+import styled from 'styled-components'
+
+const AppContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(90deg,#002F52 35%,#326589 165%);
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className='logo'>
-          <img src={logo} alt='logo'></img>
-          <p><strong>Store</strong>Books</p>
-        </div>
-      </header>
-    </div>
+    <AppContainer>
+      <Header />
+    </AppContainer>
   );
 }
 
-export default App;
+export default App
---
+++ Novas aulas/React/store-books/src/components/Input/index.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+const Input = styled.input`
+    background-color: transparent;
+    border: 1px solid #fff;
+    padding: 20px 140px;
+    border-radius: 50px;
+    width: 200px;
+    color: #fff;
+    font-size: 16px;
+    margin-bottom: 30px;
+    &::placeholder {
+        color: #fff;
+        font-size: 16px;
+    }
+`
